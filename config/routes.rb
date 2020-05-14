e63b5816a2c61515e98a67dc3a187b7ffd0e03a0
Jocelyn Wang <wangjoc@gmail.com>
2020-05-14 04:58:29 -0700

    set up user database and basic views and buttons

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
 
   resources :works
 
+
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
 end
