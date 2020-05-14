9cf21bb7ef7dd4bb5ac3266c13e0b49e277ff7ac
Jocelyn Wang <wangjoc@gmail.com>
2020-05-13 22:39:54 -0700

    create action to save data for new works

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -11,4 +11,27 @@ class WorksController < ApplicationController
     end
   end
 
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params) 
+
+    if @work.save 
+      redirect_to works_path
+      flash[:success] = "Successfully added new work: #{view_context.link_to @work.title, work_path(@work.id) }"
+      return
+    else 
+      render :new, status: :bad_request
+      return
+    end
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
+
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,9 @@
 class Work < ApplicationRecord
+  validates :category, presence: true
+  validates :title, presence: true
+  validates :creator, presence: true
+  validates :publication_year, presence: true
+  validates :description, presence: true
 
   # def category_list
   #   return works.all.map {|t| t.category}.uniq
