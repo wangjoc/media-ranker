a0d55514a2c1794661dc418fb8f1f254c5a41405
Jocelyn Wang <wangjoc@gmail.com>
2020-05-13 19:23:29 -0700

    created migration and tested ability to seed database with work data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+WORK_FILE = Rails.root.join('db', 'works-seeds.csv')
+puts "Loading raw work data from #{WORK_FILE}"
+
+work_failures = []
+CSV.foreach(WORK_FILE, :headers => true) do |row|
+  work = Work.new
+  work.category = row['category']
+  work.title = row['title']
+  work.creator = row['creator']
+  work.publication_year = row['publication_year']
+  work.description = row['description']
+  successful = work.save
+  if !successful
+    work_failures << work
+    puts "Failed to save work: #{work.inspect}"
+  else
+    puts "Created work: #{work.inspect}"
+  end
+end
+
+puts "Added #{Work.count} work records"
+puts "#{work_failures.length} works failed to save"
+
+# Since we set the primary key (the ID) manually on each of the
+# tables, we've got to tell postgres to reload the latest ID
+# values. Otherwise when we create a new record it will try
+# to start at ID 1, which will be a conflict.
+# puts "Manually resetting PK sequence on each table"
+# ActiveRecord::Base.connection.tables.each do |t|
+#   ActiveRecord::Base.connection.reset_pk_sequence!(t)
+# end
+
+puts "done"
