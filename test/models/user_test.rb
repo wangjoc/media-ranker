d5fbfcace6165954b6c4b79afed176607f6c2477
Jocelyn Wang <wangjoc@gmail.com>
2020-05-15 06:18:21 -0700

    created user validation tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  describe 'validations' do
+    before do
+      @user = users(:hatter)
+      # @book = Book.new(title: 'test book', user: user)
+    end
+
+    it 'is valid when all fields are present' do
+      result = @user.valid?
+      expect(result).must_equal true
+    end
+
+    it 'is invalid without a username' do
+      @user.username = nil
+      result = @user.valid?
+    
+      expect(result).must_equal false
+      expect(@user.errors.messages).must_include :username
+    end
+
+    it 'cannot create a user with an existing username' do
+      @new_user = User.new(username: @user.username)
+      result = @new_user.save
+
+      expect(result).must_equal false
+    end
+  end
 end
