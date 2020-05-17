8f90728934a148e1c5976b275d5cea92314c0098
Jocelyn Wang <wangjoc@gmail.com>
2020-05-16 18:12:38 -0700

    update test to have different response if logged in or not

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,16 @@
 require "test_helper"
 
 describe PagesController do
-  it "must get home" do
+  it "must get home if logged in" do
+    perform_login
+
     get pages_home_url
     must_respond_with :success
   end
+
+  it "must redirect to login page if not logged in" do
+    get pages_home_url
+    must_respond_with :redirect
+  end
 end
+
