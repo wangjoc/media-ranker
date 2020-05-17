75338f912e195aa48f031b63103279fc5bf53f43
Jocelyn Wang <wangjoc@gmail.com>
2020-05-16 18:15:34 -0700

    finished vote validation tests

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 describe Vote do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe 'validations' do
+    before do
+      @vote = votes(:hatter_vote_saucepan)
+    end
+
+    it 'is valid when all fields are present' do
+      result = @vote.valid?
+      expect(result).must_equal true
+    end
+
+    it 'is invalid without a user' do
+      @vote.user_id = nil
+      result = @vote.valid?
+    
+      expect(result).must_equal false
+      expect(@vote.errors.messages).must_include :user
+    end
+
+    it 'is invalid without a work' do
+      @vote.work_id = nil
+      result = @vote.valid?
+    
+      expect(result).must_equal false
+      expect(@vote.errors.messages).must_include :work
+    end
+  end
 end
